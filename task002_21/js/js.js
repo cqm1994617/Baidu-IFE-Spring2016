f8b3c5b9c0fdd003c0e9909d077c0f3b5e83b4c8
cqm <1025760508@qq.com>
2016-03-21 19:16:55 +0800

    ife

---
+++ task002_21/js/js.js
@@ -0,0 +1,98 @@
+(function init() {
+	var map = document.getElementById('map');
+	var boxArr = [];
+	for (var i = 0; i < 100; i++) {
+		boxArr.push('<div class="mapBox"></div>');
+	}
+	boxArr.push('<div id="mover" class="mover"></div>');
+	map.innerHTML = boxArr.join('')
+	mover.style.transform = "rotate(0deg)";
+	mover.style.left = "160px";
+	mover.style.top = "160px";
+})();
+
+(function() {
+	var mover = document.getElementById('mover');
+	var btn = document.getElementById('btn');
+	var input = document.getElementById('input');
+	
+	var myMove = {
+		face: 0,
+		turnLeft: function() {
+			--this.face;
+			mover.style.transform = "rotate(" + this.face * 90 + "deg)";
+		},
+		turnRight: function() {
+			++this.face;
+			mover.style.transform = "rotate(" + this.face * 90 + "deg)";
+		},
+		turnBack: function() {
+			this.face = this.face + 2;
+			mover.style.transform = "rotate(" + this.face * 90 + "deg)";
+		},
+		go: function() {
+			if (this.face % 4 == 3 || this.face % 4 == -1) {
+				//朝左
+				if (parseInt(mover.style.left) > 0) {
+					mover.style.left = (parseInt(mover.style.left) - 40) + 'px';
+				}
+			}
+			if (this.face % 4 == 2 || this.face % 4 == -2) {
+				//朝下
+				if (parseInt(mover.style.top) < 360) {
+					mover.style.top = (parseInt(mover.style.top) + 40) + 'px';
+				}
+			}
+			if (this.face % 4 == 1 || this.face % 4 == -3) {
+				//朝右
+				if (parseInt(mover.style.left) < 360) {
+					mover.style.left = (parseInt(mover.style.left) + 40) + 'px';
+				}
+			}
+			if (this.face % 4 == 0) {
+				//朝上
+				if (parseInt(mover.style.top) > 0) {
+					mover.style.top = (parseInt(mover.style.top) - 40) + 'px';
+				}
+			}
+		}
+	};
+	
+	function main() {
+		var value = input.value;
+		value = value.toLowerCase();
+		if (value == "go") {
+			myMove.go();
+		}
+		if (value == "tun left") {
+			myMove.turnLeft();
+		}
+		if (value == "tun right") {
+			myMove.turnRight();
+		}
+		if (value == "tun back") {
+			myMove.turnBack();
+		}
+	}
+	
+	btn.addEventListener('click', main);
+	document.getElementById('go').addEventListener('click', function(){
+		myMove.go()
+	});
+	document.getElementById('tun-left').addEventListener('click', function(){
+		myMove.turnLeft()
+	});
+	document.getElementById('tun-right').addEventListener('click', function(){
+		myMove.turnRight()
+	});
+	document.getElementById('tun-back').addEventListener('click', function(){
+		myMove.turnBack()
+	});
+
+	document.addEventListener('keydown', function(e) {
+		if (e.keyCode == 13 && input == document.activeElement) {
+			main()
+		}
+	})
+
+})()
